8ea502594fd5976df0fbbe264049b21e21fcb042
xaviercl <xcl7992@hotmail.com>
2023-12-11 11:35:40 -0500

    Migrated to vite + react + ts + firebase

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBv1a695chwPZNm3NN_zP6C8OgmZKKJDU",
+  authDomain: "nochicane-1.firebaseapp.com",
+  projectId: "nochicane-1",
+  storageBucket: "nochicane-1.appspot.com",
+  messagingSenderId: "944003543899",
+  appId: "1:944003543899:web:288108249c68bd657b607e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
